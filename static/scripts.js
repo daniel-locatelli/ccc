0afc177dea3b6bd91425d9488b4c49ebc2715684
daniel-locatelli <daniel.nlocatelli@gmail.com>
2023-10-30 00:31:09 +0100

    Added Bootstrap folder, updated Layers accordeon with settings dropdown

--- static/scripts.js
+++ static/scripts.js
@@ -2,6 +2,22 @@ paragraph = document.getElementById("content");
 
 paragraph.innerHTML = "Changed!";
 
-function changeDropdownText(text) {
+function changeDrpdwnComponentTitle(text) {
     document.getElementById('dropdownMenuComponent').textContent = text;
 }
+
+
+function changeDrpdwnMaterialText(text) {
+    document.getElementById('drpdwnMenuMat').textContent = text;
+}
+
+
+function changeDrpdwnTypeText(text) {
+    document.getElementById('drpdwnMenuType').textContent = text;
+}
+
+
+function changeDrpdwnFuncText(text) {
+    document.getElementById('drpdwnMenuFunction').textContent = text;
+}
+
