14f2db214f0f341e50ccdf6af6ddd37f5b50e25f
daniel-locatelli <daniel.nlocatelli@gmail.com>
2023-11-02 21:01:21 +0100

    Added settings container and started the component dropdown

--- static/scripts.js
+++ static/scripts.js
@@ -1,16 +1,15 @@
+// Mobile menu
+const hamburgerMenu = document.querySelector(".hamburger-icon");
+const mobileMenu = document.querySelector(".mobile-menu");
 
-
-function toggleMobileMenu() {
-    var test = document.querySelector("p");
-    
-    const hamburgerMenu = document.querySelector(".hamburger-icon");
-    hamburgerMenu.classList.toggle('color-hamburguer')
-    var mobileMenu = document.querySelector(".mobile-menu");
+hamburgerMenu.addEventListener("click", function() {
+    // Change color of hamburger icon
+    hamburgerMenu.classList.toggle('color-hamburguer');
+    // Toggle mobile menu on or of
     var currentStyle = mobileMenu.style.display;
-    
     if (currentStyle === "none" || currentStyle === "") {
         mobileMenu.style.display = "flex";
     } else {
         mobileMenu.style.display = "none";
     }
-}
+});
